0c6470a0e332168cbb2fb17219591b27dfb20841
Ashutosh Roy <rashu2879@gmail.com>
2024-12-14 05:22:39 +0530

    Array

---
+++ Array/file.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+        int a = 4;
+        int b = 3;
+        int sum = a+b;
+        int div = a/b;
+        cout<<sum<<endl;
+        cout<<div<<endl;
+}
