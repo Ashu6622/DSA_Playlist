c6dc71fa5dbcfe8fd5a367d93f552c796d62e7fa
Ashutosh Roy <rashu2879@gmail.com>
2024-12-14 05:33:38 +0530

    Update file.cpp

--- String/file.cpp
+++ String/file.cpp
@@ -5,9 +5,10 @@ int main(){
     string str = "rjne mvriuernvovnovinrv";
 
     int index = 0;
+    int ch = 'n';
 
     for(int i=0;i<str.length();i++){
-        if(str[i] == ' '){
+        if(str[i] == 'n'){
             index = i;
             break;
         }
