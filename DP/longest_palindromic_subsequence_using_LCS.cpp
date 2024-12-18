a0666f1ca39e3149878ed2b1d5e287b11ef28b63
Ashutosh Roy <rashu2879@gmail.com>
2024-12-18 09:25:12 +0530

    longest_palindromic_subsequence_using_LCS

--- DP/longest_palindromic_subsequence_using_LCS.cpp
+++ DP/longest_palindromic_subsequence_using_LCS.cpp
@@ -4,24 +4,35 @@ using namespace std;
 // using LCS to get LPS
 // make one extra string, which the reverse of main string, now perform the LCS on both the string, and the resultant length will the LPS
 
-int solve(int i, int j, string s1, string s2){
+int solve(int i, int j, string s1, string s2, vector<vector<int>> &dp){
 
         //if any one of the string gets over then return 0, no more comparision
         if(i >= s1.length() || j >= s2.length()){
             return 0;
         }
 
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+
         // if both the characters are same, then add 1 and incerase both the pointers
         if(s1[i] == s2[j]){
-            return 
+            dp[i][j] = solve(i+1,j+1, s1, s2, dp) + 1;
+            return dp[i][j];
         }
+
+        int index_i = solve(i+1, j, s1, s2, dp);
+        int index_j = solve(i, j+1, s1, s2, dp);
+
+        dp[i][j] = max(index_i, index_j);
+        return max(index_i, index_j);
 
 
 }
 
 int main(){
 
-        string s1 = "abcaa";
+        string s1 = "bbbab";
        string s2 = "";
 
        for(int i = s1.length()-1; i>=0;i--){
@@ -29,6 +40,8 @@ int main(){
             s2 += s1[i];
        }
 
-        int result = solve(0, 0,s1, s2);
+       vector<vector<int>> dp(s1.length()+1, vector<int>(s1.length(), -1));
+
+        int result = solve(0, 0,s1, s2, dp);
         cout<<result<<endl;
 }
