88272a32f8d2a0a7332c3982930070ff5f9d9a9f
Ashutosh Roy <rashu2879@gmail.com>
2024-12-25 16:28:12 +0530

    minimum_characters_to_in_front_to_make_string_palindrome

--- String/string_matching/minimum_characters_to_in_front_to_make_string_palindrome.cpp
+++ String/string_matching/minimum_characters_to_in_front_to_make_string_palindrome.cpp
@@ -1,4 +1,47 @@
-// # 3 approach
-// brute approach very important
+// # 2 approach
 // find the longest prefix which is also palindrome
 // using KMP algo
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    string str = "abcde";
+
+    string temp = str;
+    reverse(temp.begin(), temp.end());
+
+    string s = str + '$' + temp;
+
+    // construct LPS for s
+    
+    int n = s.length();
+
+    vector<int> LPS(n,0);
+
+    int i=0;
+    int j=1;
+
+    while(j<n){
+
+        // if characters are equal
+        if(s[i] == s[j]){
+                LPS[j] = i+1;
+                i++;
+                j++;
+        }
+        else{  //if not equal
+
+            if(i == 0){
+                j++;
+            }
+            else{
+                i = LPS[i-1];
+            }
+        }
+    }
+
+
+    cout<<str.length()-LPS[n-1]<<endl;
+}
