661ad4d673233da0211f86e561138719a823b89e
Ashutosh Roy <rashu2879@gmail.com>
2025-01-07 16:01:40 +0530

    Create smallest_number.cpp

---
+++ Greedy/smallest_number.cpp
@@ -0,0 +1,6 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// to find the smallest numnber of D digits
+// if D = 4, i.e 4 digit number
+// _ _ _ _ , if the number to be smallest then the first digit should always start with 1, followed with 0's and at the end non zero numbers should be placed
