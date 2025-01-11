39d39c488eaa2cd1b6f65c3d460582a896b679d2
Ashutosh Roy <rashu2879@gmail.com>
2025-01-11 06:44:08 +0530

    Update pattern_search.cpp

--- String/string_matching/pattern_search.cpp
+++ String/string_matching/pattern_search.cpp
@@ -1,3 +1,3 @@
 // find all occurance of the string
 
-// benefit of LPS is that when the characters are matching then don't go again to the forst index, instead look for the suffix which is equal to prifix
+// benefit of LPS is that when the characters are matching then don't go again to the first index, instead look for the suffix which is equal to prifix
