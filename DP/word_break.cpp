e245cd79e3b7a3ce0a8fbf47fabf2fa06b1c3115
Ashutosh Roy <rashu2879@gmail.com>
2025-01-12 22:05:16 +0530

    word_break

---
+++ DP/word_break.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+    /*break the string in such a way that ythe substring which is formed after breaking the string should be 
+    present in the dictionary vector, if not possible ten return the false;*/
+    
+int main(){
+
+}
