8ba973d47fb08815f32c8212f7cf9d04344618ef
Ashutosh Roy <rashu2879@gmail.com>
2025-01-17 04:30:17 +0530

    Create count_subarray_with_atmost_k_different_intergers.cpp

---
+++ Sliding Window/count_subarray_with_atmost_k_different_intergers.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+        vector<int> nums = {1,2,1,2,3};
+        int k = 2;
+        int count = 0;
+        int i =0;
+        int j = 0;
+
+        map<int,int> mp;
+
+        while(j < nums.size()){
+                mp[nums[j]]++;
+
+                while(mp.size() > k && j > i){
+
+                    mp[nums[i]]--;
+                    if(mp[nums[i]] == 0){
+                        mp.erase(nums[i]);
+                    }
+                    i++;
+                }
+
+                count += j - i + 1;
+                j++;
+        }
+        cout<<count<<endl;
+}
