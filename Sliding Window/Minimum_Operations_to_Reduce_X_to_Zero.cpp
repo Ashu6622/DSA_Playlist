d84686444935982ab3b44438e15fd10ea225dd1f
Ashutosh Roy <rashu2879@gmail.com>
2025-01-18 13:38:55 +0530

    Update Minimum_Operations_to_Reduce_X_to_Zero.cpp

--- Sliding Window/Minimum_Operations_to_Reduce_X_to_Zero.cpp
+++ Sliding Window/Minimum_Operations_to_Reduce_X_to_Zero.cpp
@@ -3,8 +3,59 @@ using namespace std;
 
 // Very Important Problem
 
+int longestSubarray(vector<int>& nums, int k){
+
+    if(k == 0){
+        return 0;
+    }
+
+    int i = 0;
+    int j = 0;
+    int maxlength = INT_MIN;
+    int sum = 0;
+
+    while(j < nums.size()){
+
+            sum += nums[j];
+
+            while(sum > k && j > i){
+                sum -= nums[i];
+                i++;
+            }
+
+            if(sum == k){
+                maxlength = max(maxlength, j-i+1);
+            }
+
+            j++;
+    }
+
+    return maxlength;
+
+}
+
 int main(){
 
-    vector<int> 
+    vector<int> nums = {5,6,7,8,9};
+
+    int  k = 4;
+
+    int sum = 0;
+
+    for(int i=0;i<nums.size();i++){
+        sum += nums[i];
+    }
+
+
+    int X = sum - k;
+
+    int length = longestSubarray(nums, X);
+
+    if(length == INT_MIN){
+        cout<<-1<<endl;
+        return 0;
+    }
+
+    cout<<nums.size() - length<<endl;
 
 }
