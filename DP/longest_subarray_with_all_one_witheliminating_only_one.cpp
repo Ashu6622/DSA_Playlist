4dd412999152056c7a19b4181dc9e812b8636721
Ashutosh Roy <rashu2879@gmail.com>
2025-01-20 11:14:17 +0530

    Update longest_subarray_with_all_one_witheliminating_only_one.cpp

--- DP/longest_subarray_with_all_one_witheliminating_only_one.cpp
+++ DP/longest_subarray_with_all_one_witheliminating_only_one.cpp
@@ -1,28 +1,36 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int solve(vector<int> &nums, int i, int length){
+ int solve(vector<int> &nums, int  i, int k, int length){
 
-    if(i >= nums.size()){
-        return length;
+        if(i >= nums.size()){
+            return length;
+        }
+
+
+        int maxLength = length;
+
+        if(nums[i] == 1){
+            maxLength = solve(nums, i+1, k, length+1);
+        }
+        else if(nums[i] == 0 && k > 0){
+            int take = solve(nums, i+1, k, 0);
+            int nottake = solve(nums, i+1, k-1, length);
+            maxLength = max({maxLength, take, nottake});
+        }
+
+        return maxLength;
     }
-
-    int maxLength = length;
-
-    if(nums[i] == 0){
-        int take = solve(nums, i+1, length);
-        int nottake = solve(nums, i+1, 0);
-        maxLength + max(take,nottake);
-
-    }
-        
-}
 
 int main(){
 
-    vector<int> nums = {1,1,0,1};
+    vector<int> nums = {1,1,1};
 
-    int result = solve(nums, 0, 0);
+    int result = solve(nums, 0, 1, 0);
+
+     if(result == nums.size()){
+            result = result -1;
+        }
 
     cout<<result<<endl;
 }
