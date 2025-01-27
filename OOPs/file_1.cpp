d3e66856dd2e34d34445e7ecf37f245603a48a14
Ashutosh Roy <rashu2879@gmail.com>
2025-01-27 17:48:50 +0530

    file_1

---
+++ OOPs/file_1.cpp
@@ -0,0 +1,16 @@
+ #include<bits/stdc++.h>
+ using namespace std;
+
+class Student{
+
+    public:
+    string name;
+    int roll_no;
+
+};
+
+ int main(){
+    Student S1;
+    S1.name = "Ashutosh";
+    cout<<S1.name<<endl;
+ }
