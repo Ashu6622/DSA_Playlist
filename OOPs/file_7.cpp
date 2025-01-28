22b9ffb629618b358983e30017af736a0eab4e53
Ashutosh Roy <rashu2879@gmail.com>
2025-01-28 10:08:41 +0530

    Update file_7.cpp

--- OOPs/file_7.cpp
+++ OOPs/file_7.cpp
@@ -1,6 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// constructor and Destructor
+
 class Customer{
     public:
     int roll_no;
@@ -19,4 +21,6 @@ int main(){
 
         Customer c1("Ashutosh", 123);
         Customer c2("Rahul", 456);
+        Customer * c3 = new Customer("Ayush", 987);  //destuctor is not called for this onject because, becase it dyanamically created, so to call the destructor we have to delete this object explicitly
+        delete c3;
 }
