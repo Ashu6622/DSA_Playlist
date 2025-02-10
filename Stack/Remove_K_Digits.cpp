aa8171b59d309ebda4481e3ee1fd25a673503b3a
Ashutosh Roy <rashu2879@gmail.com>
2025-02-10 14:29:23 +0530

    Create Remove_K_Digits.cpp

---
+++ Stack/Remove_K_Digits.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    string str = "1002991";
+    int k = 3;
+
+    stack<char> st;
+
+    for(int i=0;i<str.length();i++){
+
+        while(k > 0 && !st.empty() && st.top() > str[i]){
+            st.pop();
+            k--;
+        }
+
+        if(st.size() == 0 and str[i] == '0'){
+            continue;
+        }
+
+        st.push(str[i]);
+    }
+
+    while(k-- && !st.empty()){
+        st.pop();
+    }
+
+    string ans = "";
+
+    while(!st.empty()){
+        ans += st.top();
+        st.pop();
+    }
+
+    reverse(ans.begin(), ans.end());
+
+    cout<<ans<<endl;
+}
