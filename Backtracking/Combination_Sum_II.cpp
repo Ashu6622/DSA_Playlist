6ddcdb63df2d3fe5b659e9f32803df53a68ec9e5
Ashutosh Roy <rashu2879@gmail.com>
2025-02-12 16:14:57 +0530

    Update Combination_Sum_II.cpp

--- Backtracking/Combination_Sum_II.cpp
+++ Backtracking/Combination_Sum_II.cpp
@@ -13,7 +13,7 @@ void combination(int index, vector<int> &nums, int k, vector<int>& temp){
 
         for(int i=index; i<nums.size(); i++){
 
-            if(i != index && nums[i] == nums[i-1]){
+            if(i != index && nums[i] == nums[i-1]){  //to avoid the duplicate combinations
                 continue;
             }   
 
@@ -31,7 +31,7 @@ int main(){
 
     vector<int> nums = {2,5,2,1,2};
     int k = 5;
-    sort(nums.begin(), nums.end());
+    sort(nums.begin(), nums.end());  //by sorting the array duplicates elements comes at alternate indexs, so it becomes easier to eliminate the dulpicate values
     vector<int> temp;
 
     combination(0, nums, k, temp);
