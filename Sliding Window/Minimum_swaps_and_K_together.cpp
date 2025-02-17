53882771d0d5c61c96f7045aa00bcae5359114c4
Ashutosh Roy <rashu2879@gmail.com>
2025-02-18 00:40:16 +0530

    Create Minimum_swaps_and_K_together.cpp

---
+++ Sliding Window/Minimum_swaps_and_K_together.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    vector<int> nums = {2, 1, 5, 6, 3};
+    int k = 3;
+
+    int minLength = 0;
+
+    for(int i=0;i<nums.size();i++){
+        if(nums[i] <= k){
+            minLength++;
+        }
+    }
+
+    int i=0;
+    int j=0;
+    int cnt = 0;
+    int minSwap = INT_MIN;
+
+    while(j < nums.size()){
+
+        if(nums[j] <= k){
+            cnt++;
+        }
+
+        if(j - i + 1 == minLength){
+            minSwap = max(minSwap , cnt);
+            if(nums[i] <= k){
+                cnt--;
+            }
+            i++;
+        }
+
+        j++;
+    }
+
+    minSwap = minLength - minSwap;
+
+    cout<<minSwap<<endl;
+}
