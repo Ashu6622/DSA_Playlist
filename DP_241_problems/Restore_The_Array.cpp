d8df4612f547ff685156c1ceca3fe990f3a11f18
Ashutosh Roy <rashu2879@gmail.com>
2025-03-01 18:12:51 +0530

    Restore_The_Array

---
+++ DP_241_problems/Restore_The_Array.cpp
@@ -0,0 +1,38 @@
+     #include<bits/stdc++.h>
+     using namespace std;
+
+     int solve(int idx, string &S, int k){
+
+        if(idx >= S.length()){
+            return 1;
+        }
+
+        if(S[idx] == '0'){
+            return 0;
+        }
+
+        int totalCount = 0;
+        long long val = 0;
+
+        for(int i=idx; i< S.length(); i++){
+
+            int val = val*10 + (S[i] - '0');
+
+            if(val > k){
+                return totalCount;
+            }
+            int sum = solve(i+1, S, k);
+            totalCount += sum;
+        }
+        return totalCount;
+     }
+
+     int main(){
+
+        string S = "1000";
+        int k = 1000;
+
+        int result = solve(0, S, k);
+
+        cout<<result<<endl;
+     }
